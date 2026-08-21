2de915a82836598a1cda5839b7f1955cf006bceb
awakenginexe <267649137+awakenginexe@users.noreply.github.com>
2026-08-21 21:15:53 +0700

    release: v3.2.1

--- Dotnet/Program.cs
+++ Dotnet/Program.cs
@@ -81,7 +81,7 @@ namespace VRCX
             catch (Exception ex)
             {
                 logger.Error(ex, "Failed to read version file");
-                Version = "BetterVRCX v3.2.0 B 2026.08.19";
+                Version = "BetterVRCX v3.2.1 B 2026.08.19";
             }
         }
 
